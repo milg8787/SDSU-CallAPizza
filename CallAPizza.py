7d5eb203705ec8a54506cd7b1517acfd1b181618
milg8787 <milg8548@sdsu.edu>
2021-11-01 20:14:52 -0700

    added more features and success page

--- CallAPizza.py
+++ CallAPizza.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "CallAPizzaSecret"
 
 @app.route("/")
 def main(name=None):
@@ -14,6 +15,14 @@ def products(name=None):
 def order(name=None):
     return render_template('order.html', name=name)
 
-@app.route("/customerInput")
+@app.route("/customerInput", methods = ["POST", "GET"])
 def customerInput(name=None):
-    return render_template('customerInput.html', name=name)
+    if request.method == "POST":
+        result = request.form.get("CustomerFirstName")
+        return redirect(url_for("success", firstName=result))
+    else:
+        return render_template('customerInput.html', name=name)
+
+@app.route("/<firstName>")
+def success(firstName):
+    return render_template('success.html', value=firstName)
