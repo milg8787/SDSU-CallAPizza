a2b2f4833c9c4c65298c6392183f93f92807ba6e
milg8787 <milg8548@sdsu.edu>
2021-12-01 12:37:01 -0800

    added new database functionallity and the cart site

--- CallAPizza.py
+++ CallAPizza.py
@@ -21,40 +21,7 @@ def main(name=None):
 def products(name=None):
     return render_template('products.html', name=name)
 
-@app.route("/order", methods = ["POST", "GET"])
-def order(name=None):
-    conn = pymssql.connect(server='callapizza.database.windows.net:1433', user='SanDiegoAdmin', password='Tiftpasdsu1', database='callapizza')
-    cursor = conn.cursor()
-    orderList = []
-    pizzaList = []
-    
 
-    customerID = session['customerID']
-    cursor.execute("Select * from products")
-    products = cursor.fetchall()
-        
-    for item in products:
-        tempList = [item[1], item[2], item[3]]
-        pizzaList.append(tempList)
-
-    if request.method == "POST":
-        orderList.append(request.form.get)
-
-        if request.form.get("goToCart"):
-            orderInsert = """INSERT INTO orders (customerID, orderDate, orderStatus, delivery) 
-                            VALUES (%s, %s, %s, %s) """
-            # Fix delivery with a button in customerInput
-            orderInsertRecord = (customerID, datetime.datetime.now(), 0, 0)
-            cursor.execute(orderInsert, orderInsertRecord)
-            conn.commit()
-
-            orderDetailsInsert = """INSERT INTO orderDetails (orderID, productID, price, quantity, salami, ham, pepperoni, jalapenos, blackOlives, redOnions)
-                            VALUES  (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-        
-    ##cursor.execute("Insert into orders")
-        
-    conn.close()
-    return render_template('order.html', pizzaList=pizzaList)
 
 @app.route("/customerInput", methods = ["POST", "GET"])
 def customerInput(name=None):
@@ -69,23 +36,85 @@ def customerInput(name=None):
         request.form.get("address"), request.form.get("zipCode"), request.form.get("city"), request.form.get("phoneNumber"),
         request.form.get("addressComments"), request.form.get("email"))
 
-        print(customerInputRecord, file=sys.stderr)
-
         cursor.execute(customerInsertQuery, customerInputRecord)
         conn.commit()
+
+        session['firstName'] = request.form.get("customerFirstName")
 
         customerQuery = """Select customerID from customers where customerLastName = %s and customerFirstName = %s"""
         customerInput = (request.form.get("customerLastName"), request.form.get("customerFirstName"))
         cursor.execute(customerQuery, customerInput)
         customerID = cursor.fetchone()
-        print(customerID[0], file=sys.stderr)
         session['customerID'] = customerID[0]
+
+
+        orderInsert = """INSERT INTO orders (customerID, orderDate, orderStatus, delivery) 
+                            VALUES (%s, %s, %s, %s) """
+            # Fix delivery with a button in customerInput
+        orderInsertRecord = (customerID[0], datetime.datetime.now(), 0, request.form.get("isDelivery"))
+        cursor.execute(orderInsert, orderInsertRecord)
+        conn.commit()
 
         conn.close()
         return redirect(url_for("order", none=None))
     
     return render_template('customerInput.html', name=name)
 
-@app.route("/<firstName>")
-def success(firstName):
+
+@app.route("/order", methods = ["POST", "GET"])
+def order(name=None):
+    conn = pymssql.connect(server='callapizza.database.windows.net:1433', user='SanDiegoAdmin', password='Tiftpasdsu1', database='callapizza')
+    cursor = conn.cursor()
+    orderList = []
+    pizzaList = []
+    
+
+    cursor.execute("Select * from products")
+    products = cursor.fetchall()
+        
+    for item in products:
+        tempList = [item[1], item[2], item[3]]
+        pizzaList.append(tempList)
+
+    if request.method == "POST":
+        orderList.append(request.form.get)
+
+        if request.form.get("goToCart"):
+            print(orderList, file=sys.stderr)
+
+            orderDetailsInsert = """INSERT INTO orderDetails (orderID, productID, price, quantity, salami, ham, pepperoni, jalapenos, blackOlives, redOnions)
+                            VALUES  (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+
+        
+    ##cursor.execute("Insert into orders")
+        
+    conn.close()
+    return render_template('order.html', pizzaList=pizzaList)
+
+
+@app.route("/cart")
+def cart(name=None):
+    #conn = pymssql.connect(server='callapizza.database.windows.net:1433', user='SanDiegoAdmin', password='Tiftpasdsu1', database='callapizza')
+    #cursor = conn.cursor()
+
+    #orderDetailsQuery = """Select price, quantiy, salami, ham, pepperoni, jalapenos, blackOlives, redOnions from orderDetails where orderID = %s"""
+    #orderDetailsInput = (session['orderID'])
+    #cursor.execute(orderDetailsQuery, orderDetailsInput)
+
+    orderItems = [("Pizza Salami", "Medium", "3", "Ham, Salami, Jalapenos, Pepperoni, black olives, red onions", "14.50"), ("Pizza Salami", "Small", "1", "AddtionalItem", "15.00"), 
+    ("Pizza Ham", "Large", "2", "AddtionalItem", "13.00")]
+
+    if request.method == "POST":
+        return redirect(url_for("success", none=None))
+
+
+    return render_template('cart.html', orderList=orderItems)
+
+
+
+@app.route("/success")
+def success(name=None):
+    #firstName = session['firstName']
+    firstName = "hugo"
     return render_template('success.html', value=firstName)
+
