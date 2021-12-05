6f39d43daf3af3c34b26479da343a474eb9c8abd
milg8787 <milg8548@sdsu.edu>
2021-12-04 18:27:30 -0800

    same commit

--- CallAPizza.py
+++ CallAPizza.py
@@ -1,3 +1,4 @@
+from itertools import count
 from os import SEEK_CUR
 import re
 from flask import Flask, render_template, url_for, request, redirect, session
@@ -7,12 +8,13 @@ import uuid, sys, datetime
 from werkzeug.wrappers import response
 from customerTO import customerTO
 
-#import pymssql
-import pyodbc
+import pymssql
+# import pyodbc
 
 app = Flask(__name__)
 app.secret_key = "CallAPizzaSecret"
 
+orderList = []
 
 @app.route("/")
 def main(name=None):
@@ -56,6 +58,13 @@ def customerInput(name=None):
         cursor.execute(orderInsert, orderInsertRecord)
         conn.commit()
 
+        orderQuery = """Select orderID from orders where customerID = %s order by orderDate desc"""
+        orderInput = (session['customerID'])
+        cursor.execute(orderQuery, orderInput)
+        orderID = cursor.fetchone()
+        session['orderID'] = orderID[0]
+
+
         conn.close()
         return redirect(url_for("order", none=None))
     
@@ -69,7 +78,6 @@ def order(name=None):
     conn = connectToDatabase()
     cursor = conn.cursor()
 
-    orderList = []
     pizzaList = []
     
 
@@ -77,20 +85,31 @@ def order(name=None):
     products = cursor.fetchall()
         
     for item in products:
-        tempList = [item[1], item[2], item[3]]
+        tempList = [item[0], item[1], item[2], item[3]]
         pizzaList.append(tempList)
 
     if request.method == "POST":
-        orderList.append(request.form.get)
 
-        if request.form.get("goToCart"):
-            print(orderList[0], file=sys.stderr)
+        if request.form['addToCart'] == 'Add to cart':
+            additionalItemList = [request.form.get("checkBoxSalami"), request.form.get("checkBoxHam"), request.form.get("checkBoxPepperoni"), 
+            request.form.get("checkBoxJalapenos"), request.form.get("checkBoxBlackOlives"), request.form.get("checkBoxRedOnions")]
 
-            orderDetailsInsert = """INSERT INTO orderDetails (orderID, productID, price, quantity, salami, ham, pepperoni, jalapenos, blackOlives, redOnions)
-                            VALUES  (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+            price = float(request.form.get("basicPrice"))
 
+            if price is not None:
+                if request.form.get("size") == "small":
+                    price = (price + 0.5 * additionalItemList.count("on")) * float(request.form.get("quantity")) - 3
+                elif request.form.get("size") == "large":
+                    price = (price + 0.5 * additionalItemList.count("on")) * float(request.form.get("quantity")) + 3
+                else:
+                    price = (price + 0.5 * additionalItemList.count("on")) * float(request.form.get("quantity"))
+                
+            orderList.append([session['orderID'], request.form.get("productID"), price, request.form.get("size"), request.form.get("quantity"), 
+            replaceValue(request.form.get("checkBoxSalami")), replaceValue(request.form.get("checkBoxHam")),
+            replaceValue(request.form.get("checkBoxPepperoni")), replaceValue(request.form.get("checkBoxJalapenos")), 
+            replaceValue(request.form.get("checkBoxBlackOlives")), replaceValue(request.form.get("checkBoxRedOnions"))])
         
-    ##cursor.execute("Insert into orders")
+        print(orderList, file=sys.stderr)
         
     conn.close()
     return render_template('order.html', pizzaList=pizzaList)
@@ -98,7 +117,7 @@ def order(name=None):
 
 
 
-@app.route("/cart")
+@app.route("/cart", methods = ["POST", "GET"])
 def cart(name=None):
     #conn, cursor = connectToDatabase())
 
@@ -106,11 +125,11 @@ def cart(name=None):
     #orderDetailsInput = (session['orderID'])
     #cursor.execute(orderDetailsQuery, orderDetailsInput)
 
-    orderItems = [("Pizza Salami", "Medium", "3", "Ham, Salami, Jalapenos, Pepperoni, black olives, red onions", "14.50"), ("Pizza Salami", "Small", "1", "AddtionalItem", "15.00"), 
+    orderList = [("Pizza Salami", "Medium", "3", "Ham, Salami, Jalapenos, Pepperoni, black olives, red onions", "14.50"), ("Pizza Salami", "Small", "1", "AddtionalItem", "15.00"), 
     ("Pizza Ham", "Large", "2", "AddtionalItem", "13.00")]
 
-    if request.method == "POST":
-        return redirect(url_for("success", none=None))
+    # if request.method == "POST":
+    #     return redirect(url_for("success", none=None))
 
 
     return render_template('cart.html', orderList=orderItems)
@@ -121,8 +140,15 @@ def cart(name=None):
 def success(name=None):
     #firstName = session['firstName']
     firstName = "hugo"
+    orderList = []
     return render_template('success.html', value=firstName)
 
+def replaceValue(value):
+    if value == "on":
+        return 1
+    else:
+        return 0
+    
 
 
 ## Use this as mac OS
@@ -134,17 +160,12 @@ def connectToDatabase():
     return conn
 
 
-## Use this as windows and linux
-def connectToDatabase():
-    conn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};'
-    'SERVER=tcp:callapizza.database.windows.net,1433;'
-    'DATABASE=CallAPizza;Uid=SanDiegoAdmin;'
-    'PWD=Tiftpasdsu1;'
-    'ENCRYPT=yes;TrustServerCertificate=no;'
-    'Connection Timeout=30;')
-    return conn
-
-
-# conn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};'
-#                       'SERVER=tcp:nameServer.database.windows.net,1433;'
-#                       'DATABASE=Name database; UID=name; PWD=password;')
+# ## Use this as windows and linux
+# def connectToDatabase():
+#     conn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};'
+#     'SERVER=tcp:callapizza.database.windows.net,1433;'
+#     'DATABASE=CallAPizza;Uid=SanDiegoAdmin;'
+#     'PWD=Tiftpasdsu1;'
+#     'ENCRYPT=yes;TrustServerCertificate=no;'
+#     'Connection Timeout=30;')
+#     return conn
